9d000abc35f37cf11dbf03f2058eb7410aa0b91d
Jonathan Captanis <captainjonoo@gmail.com>
2020-10-08 22:58:17 +1100

    Adding standard Xamarin Mac Cocoa App template and removing unused files

---
+++ Chromely.XamMac/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using Caliburn.Light;
+using Chromely.Core;
+using Chromely.Core.Configuration;
+using Chromely.Core.Host;
+using Chromely.Core.Infrastructure;
+using Chromely.Core.Logging;
+using Chromely.Core.Network;
+using Chromely.Windows;
+
+namespace Chromely.XamMac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
